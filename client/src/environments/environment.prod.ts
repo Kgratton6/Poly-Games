0ffc86444db74843a1414a39824ec5438268a8f2
Kevin Gratton <kevin-santiago.gratton-fournier@polymtl.ca>
2024-03-23 22:45:57 -0400

    changement de enviromenet

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    serverUrl: 'myServerURL', // TODO: Add remote server URL
+    serverUrl: 'https://kgratton6.pythonanywhere.com', // TODO: Add remote server URL
 };
