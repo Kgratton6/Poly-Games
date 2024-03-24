ec354ca2153d8ef9b74c112e9daf5bc386a16292
Kevin Gratton <kevin-santiago.gratton-fournier@polymtl.ca>
2024-03-23 23:01:12 -0400

    ajout des static sur le serveur

--- server/server/settings.py
+++ server/server/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # commandes
 #python manage.py createsuperuser
@@ -131,6 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
