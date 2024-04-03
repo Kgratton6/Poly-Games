cd486e3231414ef9c2026a7e7798349c21e01268
Kevin Gratton <kevin-santiago.gratton-fournier@polymtl.ca>
2024-04-02 20:19:45 -0400

    ajouts des channels

---
+++ client/src/app/services/socket.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class SocketService {
+    // private subject: Rx.Subject<MessageEvent>;
+    // constructor() {}
+}
