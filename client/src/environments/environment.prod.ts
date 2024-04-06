a8782880d6a92ef95691d07c4a07fc41c8a157bd
Kevin Gratton <kevin-santiago.gratton-fournier@polymtl.ca>
2024-04-06 15:27:18 -0400

    remise en arriere de environement

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
     production: true,
-    // serverUrl: 'https://kgratton6.pythonanywhere.com',
-    // socketUrl: 'wss://kgratton6.pythonanywhere.com',
-    serverUrl: 'http://192.168.145.1:8000',
-    socketUrl: 'ws://192.168.145.1:8000',
+    serverUrl: 'https://kgratton6.pythonanywhere.com',
+    socketUrl: 'wss://kgratton6.pythonanywhere.com',
 };
