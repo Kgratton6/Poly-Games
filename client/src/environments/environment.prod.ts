ad6d8b6fd53bb8b5345be70df97984ffe3996098
Kevin Gratton <kevin-santiago.gratton-fournier@polymtl.ca>
2024-04-06 15:21:46 -0400

    changement

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
     // serverUrl: 'https://kgratton6.pythonanywhere.com',
     // socketUrl: 'wss://kgratton6.pythonanywhere.com',
-    serverUrl: 'https://192.168.145.1:8000',
-    socketUrl: 'wss://192.168.145.1:8000',
+    serverUrl: 'http://192.168.145.1:8000',
+    socketUrl: 'ws://192.168.145.1:8000',
 };
