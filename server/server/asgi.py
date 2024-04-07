f0e3f51be68f1e1ec277b2c72661f186734a120f
Kevin Gratton <kevin-santiago.gratton-fournier@polymtl.ca>
2024-04-06 20:12:04 -0400

    changment pour deploiment serveur

--- server/server/asgi.py
+++ server/server/asgi.py
@@ -16,7 +16,7 @@
 import os
 import django
 from channels.routing import get_default_application
+from server.routing import *
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
-django.setup()
 application = get_default_application()
