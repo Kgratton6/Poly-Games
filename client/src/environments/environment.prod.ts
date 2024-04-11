258c7d78ce5bffc050743fa4afd18dab271e2650
Kevin Gratton <kevin-santiago.gratton-fournier@polymtl.ca>
2024-04-10 20:00:03 -0400

    changement encore enviromenet

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    serverUrl: 'https://poly-games-server.site:8000',
-    socketUrl: 'wss://poly-games-server.site:8000/ws',
+    serverUrl: 'https://poly-games-server.site',
+    socketUrl: 'wss://poly-games-server.site/ws',
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -7,9 +7,9 @@ export const environment = {
     // serverUrl: 'http://127.0.0.1:8000',
     // socketUrl: 'ws://127.0.0.1:8000/ws',
     // serverUrl: 'http://142.93.148.99:8000', // works
-    // socketUrl: 'ws://142.93.148.99/ws:8000', // works
-    serverUrl: 'http://poly-games-server.site:8000', // works
-    socketUrl: 'ws://poly-games-server.site:8000/ws', // woent work
+    // socketUrl: 'ws://142.93.148.99:8000/ws', // works
+    serverUrl: 'http://poly-games-server.site', // works
+    socketUrl: 'ws://poly-games-server.site/ws', // works
 };
 
 /*
